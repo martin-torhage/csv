ce4ee3e5ec01ae1dff7967af8ce1e5580481c100
Martin Torhage <martin@campanja.com>
2015-04-14 21:16:15 +0200

    Add csv:decode_fold/3

--- src/csv.erl
+++ src/csv.erl
@@ -1,5 +1,27 @@
 -module(csv).
--export([testa/0]).
+-export([decode_fold/3]).
 
-testa() ->
-    <<"It's working4">>.
+decode_fold(Folder, AccIn, Csv) when is_binary(Csv) ->
+    decode_fold1(Folder, AccIn, {undefined, Csv}).
+
+% Internal
+
+decode_fold1(Folder, Acc, {undefined, Csv}) ->
+    Parser = csv_parser:init(),
+    decode_fold1(Folder, Acc, {Parser, Csv});
+decode_fold1(Folder, Acc, {Parser, <<>>}) ->
+    Rows = csv_parser:close(Parser),
+    lists:foldl(Folder, Acc, Rows);
+decode_fold1(Folder, Acc, {Parser, Csv}) ->
+    {CsvHead, CsvTail} = binary_split_by_size(Csv, 4096),
+    NewAcc = lists:foldl(Folder, Acc, csv_parser:parse(Parser, CsvHead)),
+    decode_fold1(Folder, NewAcc, {Parser, CsvTail}).
+
+binary_split_by_size(Bin, Size) ->
+    case Size >= byte_size(Bin) of
+        true ->
+            {Bin, <<>>};
+        false ->
+            {binary:part(Bin, 0, Size),
+             binary:part(Bin, Size, byte_size(Bin) - Size)}
+    end.
