0a25f154b4335bbe4996e6d1ade01dda5736a7b1
Martin Torhage <martin.torhage@gmail.com>
2016-11-23 00:04:40 +0100

    Return only columns asked for from NIF
    Add a new interface for specifying which columns to return from the
    NIF for both performance reasons but also for convenience.

--- c_src/csv_parser_nif.c
+++ c_src/csv_parser_nif.c
@@ -35,15 +35,22 @@ struct csv_buffer {
   int consumed;
 };
 
+struct capture {
+  int *indexes_ptr;
+  int size;
+};
+
 struct state {
   struct csv_parser parser;
   struct row_buffer row_buffer;
   struct csv_buffer csv_buffer;
+  struct capture capture;
 };
 
 struct callback_state {
   struct out_buffer out_buffer;
   struct row_buffer *row_buffer_ptr;
+  struct capture *capture_ptr;
   ErlNifEnv* env_ptr;
 };
 
@@ -79,6 +86,21 @@ struct column empty_column() {
   return col;
 }
 
+bool is_output_column(struct callback_state* cb_state_ptr, int col_i) {
+  int i;
+
+  if (cb_state_ptr->capture_ptr->indexes_ptr == NULL) {
+    return true;
+  } else {
+    for (i = 0; i < cb_state_ptr->capture_ptr->size; i++) {
+      if (col_i == cb_state_ptr->capture_ptr->indexes_ptr[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
 void ensure_row_buffer_space(struct row_buffer *row_buffer_ptr) {
   struct column *new_cols_ptr;
   int new_allocated_n;
@@ -104,11 +126,44 @@ static void add_value(void *data_ptr, int size,
   struct row_buffer *row_buffer_ptr = cb_state_ptr->row_buffer_ptr;
   struct column *column_ptr;
   ensure_row_buffer_space(row_buffer_ptr);
-  column_ptr = &row_buffer_ptr->cols_ptr[row_buffer_ptr->cols_used];
-  ensure_column_size(column_ptr, size);
-  column_ptr->data_size = size;
-  memcpy(column_ptr->data_ptr, data_ptr, size);
+  if (is_output_column(cb_state_ptr, row_buffer_ptr->cols_used)) {
+    column_ptr = &row_buffer_ptr->cols_ptr[row_buffer_ptr->cols_used];
+    ensure_column_size(column_ptr, size);
+    column_ptr->data_size = size;
+    memcpy(column_ptr->data_ptr, data_ptr, size);
+  }
   row_buffer_ptr->cols_used++;
+}
+
+static int make_output_terms(struct callback_state* cb_state_ptr,
+                             ERL_NIF_TERM *out_terms_ptr)
+{
+  ErlNifEnv* env_ptr = cb_state_ptr->env_ptr;
+  struct row_buffer *row_buffer_ptr = cb_state_ptr->row_buffer_ptr;
+  struct capture *capture_ptr = cb_state_ptr->capture_ptr;
+  int cols_used = row_buffer_ptr->cols_used;
+  int out_i;
+  int capture_i;
+
+  if (capture_ptr->indexes_ptr == NULL) {
+    for (out_i = 0; out_i < cols_used; out_i++) {
+      out_terms_ptr[out_i] =
+        enif_make_string_len(env_ptr,
+                             row_buffer_ptr->cols_ptr[out_i].data_ptr,
+                             row_buffer_ptr->cols_ptr[out_i].data_size,
+                             ERL_NIF_LATIN1);
+    }
+  } else {
+    for (out_i = 0; out_i < capture_ptr->size; out_i++) {
+      capture_i = capture_ptr->indexes_ptr[out_i];
+      out_terms_ptr[out_i] =
+        enif_make_string_len(env_ptr,
+                             row_buffer_ptr->cols_ptr[capture_i].data_ptr,
+                             row_buffer_ptr->cols_ptr[capture_i].data_size,
+                             ERL_NIF_LATIN1);
+    }
+  }
+  return out_i;
 }
 
 static void add_row(struct callback_state* cb_state_ptr)
@@ -116,19 +171,14 @@ static void add_row(struct callback_state* cb_state_ptr)
   struct out_buffer *out_buffer_ptr = &(cb_state_ptr->out_buffer);
   struct row_buffer *row_buffer_ptr = cb_state_ptr->row_buffer_ptr;
   ErlNifEnv* env_ptr = cb_state_ptr->env_ptr;
-  ERL_NIF_TERM cols[row_buffer_ptr->cols_used];
-  int i;
-  int cols_used = row_buffer_ptr->cols_used;
+  ERL_NIF_TERM out_cells[row_buffer_ptr->cols_used];
+  int out_cells_used;
+
   if (out_buffer_ptr->row_n < MAX_ROWS_PER_BATCH) {
-    for (i = 0; i < cols_used; i++) {
-      cols[i] = enif_make_string_len(env_ptr,
-                                     row_buffer_ptr->cols_ptr[i].data_ptr,
-                                     row_buffer_ptr->cols_ptr[i].data_size,
-                                     ERL_NIF_LATIN1);
-    }
+    out_cells_used = make_output_terms(cb_state_ptr, out_cells);
     row_buffer_ptr->cols_used = 0;
     out_buffer_ptr->rows[out_buffer_ptr->row_n] =
-      enif_make_list_from_array(env_ptr, cols, cols_used);
+      enif_make_list_from_array(env_ptr, out_cells, out_cells_used);
     out_buffer_ptr->row_n++;
   }
 }
@@ -168,6 +218,10 @@ void init_csv_buffer(struct csv_buffer *csv_buffer_ptr)
   csv_buffer_ptr->size = 0;
 }
 
+void init_capture(struct capture *capture_ptr) {
+  capture_ptr->indexes_ptr = NULL;
+}
+
 static struct state* init_state()
 {
   struct state* state_ptr = enif_alloc_resource(state_type,
@@ -175,8 +229,56 @@ static struct state* init_state()
   if (state_ptr != NULL) {
     init_row_buffer(&state_ptr->row_buffer);
     init_csv_buffer(&state_ptr->csv_buffer);
+    init_capture(&state_ptr->capture);
   }
   return state_ptr;
+}
+
+static bool extract_capture_list(ErlNifEnv* env_ptr, ERL_NIF_TERM list,
+                                 int len, int *dst_ptr)
+{
+  int i;
+  ERL_NIF_TERM head;
+  ERL_NIF_TERM tail;
+  int item;
+
+  tail = list;
+  for (i = 0; i < len; i++) {
+    if (!enif_get_list_cell(env_ptr, tail, &head, &tail)) {
+      return false;
+    }
+    if (!enif_get_int(env_ptr, head, &item)) {
+      return false;
+    }
+    if (item < 0) {
+      return false;
+    }
+    dst_ptr[i] = item;
+  }
+  return true;
+}
+
+static bool update_capture(ErlNifEnv* env_ptr, struct state *state_ptr,
+                           ERL_NIF_TERM list) {
+  unsigned len;
+  int *indexes_new_ptr;
+  struct capture *capture_ptr;
+
+  if (!enif_get_list_length(env_ptr, list, &len)) {
+    return false;
+  } else {
+    indexes_new_ptr = enif_alloc(len * sizeof(int));
+    if (!extract_capture_list(env_ptr, list, len, indexes_new_ptr)) {
+      enif_free(indexes_new_ptr);
+      return false;
+    } else {
+      capture_ptr = &(state_ptr->capture);
+      enif_free(capture_ptr->indexes_ptr);
+      capture_ptr->indexes_ptr = indexes_new_ptr;
+      capture_ptr->size = len;
+      return true;
+    }
+  }
 }
 
 static void init_callback_state(struct callback_state *cb_state_ptr,
@@ -185,9 +287,11 @@ static void init_callback_state(struct callback_state *cb_state_ptr,
   cb_state_ptr->env_ptr = env_ptr;
   cb_state_ptr->out_buffer.row_n = 0;
   cb_state_ptr->row_buffer_ptr = &(state_ptr->row_buffer);
+  cb_state_ptr->capture_ptr = &(state_ptr->capture);
 }
 
-bool is_csv_buffer_empty(struct csv_buffer *csv_buffer_ptr) {
+bool is_csv_buffer_empty(struct csv_buffer *csv_buffer_ptr)
+{
   if (csv_buffer_ptr->data_ptr == NULL ||
       csv_buffer_ptr->size == 0 ||
       csv_buffer_ptr->consumed >= csv_buffer_ptr->size) {
@@ -197,7 +301,9 @@ bool is_csv_buffer_empty(struct csv_buffer *csv_buffer_ptr) {
   }
 }
 
-void get_csv_chunk(struct csv_chunk *chunk_ptr, struct state *state_ptr) {
+void get_csv_chunk(struct csv_chunk *chunk_ptr, struct state *state_ptr,
+                   int max_len)
+{
   struct csv_buffer *csv_buffer_ptr;
   int bytes_read;
 
@@ -206,7 +312,7 @@ void get_csv_chunk(struct csv_chunk *chunk_ptr, struct state *state_ptr) {
     chunk_ptr->size = 0;
   } else {
     bytes_read = min(csv_buffer_ptr->size - csv_buffer_ptr->consumed,
-                     MAX_PARSE_SIZE);
+                     max_len);
     chunk_ptr->size = bytes_read;
     chunk_ptr->data_ptr = csv_buffer_ptr->data_ptr + csv_buffer_ptr->consumed;
     csv_buffer_ptr->consumed += bytes_read;
@@ -324,6 +430,59 @@ static ERL_NIF_TERM feed(ErlNifEnv* env_ptr, int argc,
   }
 }
 
+static ERL_NIF_TERM set_capture(ErlNifEnv* env_ptr, int argc,
+                                const ERL_NIF_TERM argv[])
+{
+  struct state *state_ptr;
+
+  if (argc != 2) {
+    return enif_make_badarg(env_ptr);
+  }
+  if (!enif_get_resource(env_ptr, argv[0], state_type, (void**) &state_ptr)) {
+    return enif_make_badarg(env_ptr);
+  }
+  if (!update_capture(env_ptr, state_ptr, argv[1])) {
+    return enif_make_badarg(env_ptr);
+  }
+
+  return enif_make_atom(env_ptr, "ok");
+}
+
+static ERL_NIF_TERM parse_one_row(ErlNifEnv* env_ptr, int argc,
+                                  const ERL_NIF_TERM argv[])
+{
+  struct state *state_ptr;
+  struct csv_parser *parser_ptr;
+  struct callback_state cb_state;
+  struct csv_chunk chunk;
+
+  if (argc != 1) {
+    return enif_make_badarg(env_ptr);
+  }
+  if (!enif_get_resource(env_ptr, argv[0], state_type, (void**) &state_ptr)) {
+    return enif_make_badarg(env_ptr);
+  }
+  parser_ptr = &(state_ptr->parser);
+  init_callback_state(&cb_state, env_ptr, state_ptr);
+
+  while (cb_state.out_buffer.row_n == 0) {
+    get_csv_chunk(&chunk, state_ptr, 1);
+    if (chunk.size == 0) {
+      return enif_make_tuple2(env_ptr,
+                              enif_make_atom(env_ptr, "error"),
+                              enif_make_atom(env_ptr, "eob"));
+    } else {
+      if (csv_parse(parser_ptr, chunk.data_ptr, (size_t) chunk.size,
+                    column_callback, row_callback, &cb_state) != chunk.size) {
+        return error2(env_ptr,
+                      "csv_parse failed",
+                      csv_strerror(csv_error(parser_ptr)));
+      }
+    }
+  }
+  return ok_tuple(env_ptr, make_output(&cb_state));
+}
+
 static ERL_NIF_TERM parse(ErlNifEnv* env_ptr, int argc,
                           const ERL_NIF_TERM argv[])
 {
@@ -340,7 +499,7 @@ static ERL_NIF_TERM parse(ErlNifEnv* env_ptr, int argc,
   }
   parser_ptr = &(state_ptr->parser);
 
-  get_csv_chunk(&chunk, state_ptr);
+  get_csv_chunk(&chunk, state_ptr, MAX_PARSE_SIZE);
   if (chunk.size == 0) {
     return enif_make_tuple2(env_ptr,
                             enif_make_atom(env_ptr, "error"),
@@ -363,6 +522,8 @@ static ErlNifFunc nif_funcs[] =
     {"init", 0, init},
     {"close", 1, close},
     {"feed", 2, feed},
+    {"set_capture", 2, set_capture},
+    {"parse_one_row", 1, parse_one_row},
     {"parse", 1, parse},
   };
 
@@ -385,15 +546,24 @@ void row_buffer_dtor(struct row_buffer *row_buffer_ptr) {
   }
   enif_free(row_buffer_ptr->cols_ptr);
 }
+
+void capture_dtor(struct capture *capture_ptr) {
+  if (capture_ptr->indexes_ptr != NULL) {
+    enif_free(capture_ptr->indexes_ptr);
+  }
+}
+
 void state_dtor(ErlNifEnv* env_ptr, void* obj_ptr)
 {
   struct state* state_ptr = (struct state*) obj_ptr;
   struct csv_parser *parser_ptr = &(state_ptr->parser);
   struct csv_buffer *csv_buffer_ptr = &(state_ptr->csv_buffer);
   struct row_buffer *row_buffer_ptr = &(state_ptr->row_buffer);
+  struct capture *capture_ptr = &(state_ptr->capture);
   csv_free(parser_ptr);
   csv_buffer_dtor(csv_buffer_ptr);
   row_buffer_dtor(row_buffer_ptr);
+  capture_dtor(capture_ptr);
 }
 
 static int load(ErlNifEnv* env_ptr, void** priv, ERL_NIF_TERM info)
--- test/csv_tests.erl
+++ test/csv_tests.erl
@@ -7,7 +7,9 @@ csv_test_() ->
      fun setup/0,
      fun teardown/1,
      [{"Should decode binary",
-       ?_test(decode_binary())}]}.
+       ?_test(decode_binary())},
+      {"Should decode only selected columns",
+       ?_test(decode_selected_columns())}]}.
 
 setup() ->
     ok.
@@ -23,4 +25,21 @@ decode_binary() ->
                      [Row | Acc]
              end,
     Actual = lists:reverse(csv:decode_binary_fold(Folder, [], Csv)),
+    ?assertEqual(Expected, Actual).
+
+decode_selected_columns() ->
+    Csv = <<"h1v1,h1v2,h1v3\n"
+            "h2v1,h2v2,h2v3\n"
+            "r1v1,r1v2,r1v3\n",
+            "r2v1,r2v2,r2v3">>,
+    Expected = [["r1v3", "r1v3", "r1v1"],
+                ["r2v3", "r2v3", "r2v1"]],
+    FolderMaker = fun(Header1, Header2) ->
+                          ?assertEqual(["h1v1", "h1v2", "h1v3"], Header1),
+                          ?assertEqual(["h2v1", "h2v2", "h2v3"], Header2),
+                          {fun (Row, Acc) -> [Row | Acc] end,
+                           [2, 2, 0]}
+                  end,
+    Actual = lists:reverse(
+               csv:decode_binary_fold({maker, FolderMaker}, [], Csv)),
     ?assertEqual(Expected, Actual).
