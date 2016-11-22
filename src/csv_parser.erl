d2b5e41ae36894bcb34eb0c01c2ff2bfbeb38e09
Martin Torhage <martin.torhage@gmail.com>
2016-11-23 00:04:40 +0100

    Return only columns asked for from NIF
    Add a new interface for specifying which columns to return from the
    NIF for both performance reasons but also for convenience.

--- src/csv_parser.erl
+++ src/csv_parser.erl
@@ -4,6 +4,8 @@
 -export([init/0,
          close/1,
          feed/2,
+         set_capture/2,
+         parse_one_row/1,
          parse/1]).
 
 init() ->
@@ -13,6 +15,12 @@ close(_) ->
     erlang:nif_error(nif_not_loaded).
 
 feed(_, _) ->
+    erlang:nif_error(nif_not_loaded).
+
+set_capture(_, _) ->
+    erlang:nif_error(nif_not_loaded).
+
+parse_one_row(_) ->
     erlang:nif_error(nif_not_loaded).
 
 parse(_) ->
