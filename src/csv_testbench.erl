293d12ebdaaa6cdd4f5ffad929451dd9d6954d69
Martin Torhage <martin@campanja.com>
2016-12-06 00:54:23 +0100

    Use selective column decoding in testbench

--- src/csv_testbench.erl
+++ src/csv_testbench.erl
@@ -6,15 +6,34 @@ run() ->
 
 run(Filename) ->
     {ok, Csv} = file:read_file(filename:join([code:priv_dir(csv), Filename])),
+
+    FolderMaker = fun (_) ->
+                          {fun(Row, Acc) -> [Row | Acc] end,
+                           [5, 5, 1]}
+                  end,
+    Generator = make_generator(Csv),
+
     Timer = start_utimer(),
-    Decoded = csv:decode_binary(Csv),
+
+    %%Decoded = csv:decode_binary(Csv),
+    Decoded = csv:decode_fold({maker, FolderMaker}, [], Generator),
 
     RunTimeUs = stop_utimer(Timer),
     #{run_time_ms => round(RunTimeUs / 1000),
       decoded_rows => length(Decoded),
-      us_per_row => divide_and_round(RunTimeUs, length(Decoded))}.
+      us_per_row => divide_and_round(RunTimeUs, length(Decoded)),
+      rows_per_second => round(length(Decoded) / (RunTimeUs / 1000000)),
+      first_row => hd(Decoded)}.
 
 %% Internal
+
+make_generator(Csv) ->
+    {fun generator/1, Csv}.
+
+generator(done) ->
+    done;
+generator(Csv) ->
+    {Csv, done}.
 
 start_utimer() ->
     erlang:system_time(micro_seconds).
