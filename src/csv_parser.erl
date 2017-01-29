260a3afaae3289fe3685b58cf624fe05e0b7215e
Martin Torhage <martin.torhage@gmail.com>
2017-01-29 18:45:54 +0100

    Add support for tab delimiters

--- src/csv_parser.erl
+++ src/csv_parser.erl
@@ -1,14 +1,14 @@
 -module(csv_parser).
 -on_load(init_nif/0).
 
--export([init/0,
+-export([init/1,
          close/1,
          feed/2,
          set_capture/2,
          parse_one_row/1,
          parse/1]).
 
-init() ->
+init(_) ->
     erlang:nif_error(nif_not_loaded).
 
 close(_) ->
