2181b78a69639b4af534cd23d995569de7adcda6
Martin Torhage <martin.torhage@gmail.com>
2017-01-31 00:39:38 +0100

    Add test for decoding varying chunk sizes

--- test/csv_tests.erl
+++ test/csv_tests.erl
@@ -13,7 +13,9 @@ csv_test_() ->
       {"Should decode tab separated",
        ?_test(decode_tabbed_binary())},
       {"Should decode only selected columns",
-       ?_test(decode_selected_columns())}]}.
+       ?_test(decode_selected_columns())},
+      {"Should decode small batches",
+       ?_test(decode_small_batches())}]}.
 
 setup() ->
     ok.
@@ -63,3 +65,43 @@ decode_selected_columns() ->
     Actual = lists:reverse(
                csv:decode_binary_fold({maker, FolderMaker}, [], Csv)),
     ?assertEqual(Expected, Actual).
+
+decode_small_batches() ->
+    Data = generate_data(6, 1000),
+    Decoded = csv:decode_fold(fun(Row, Acc) -> [Row | Acc] end,
+                              [],
+                              {fun chunk_generator/1, encode_csv(Data)},
+                              [{return, binary}]),
+    ?assertEqual(Data, lists:reverse(Decoded)).
+
+generate_data(Cols, Rows) ->
+    [generate_row(RowI, Cols) || RowI <- lists:seq(1, Rows)].
+
+generate_row(RowI, Cols) ->
+    [iolist_to_binary(["r", integer_to_list(RowI), "c", integer_to_list(ColI)])
+     || ColI <- lists:seq(1, Cols)].
+
+encode_csv(Matrix) ->
+    iolist_to_binary([encode_csv_row(Row) || Row <- Matrix]).
+
+encode_csv_row(Row) ->
+    encode_csv_row(Row, []).
+
+encode_csv_row([], Acc) ->
+    lists:reverse(Acc);
+encode_csv_row([Col], Acc) ->
+    NewAcc = [["\"", Col, "\"\n"] | Acc],
+    encode_csv_row([], NewAcc);
+encode_csv_row([Col | Rest], Acc) ->
+    NewAcc = [["\"", Col, "\","] | Acc],
+    encode_csv_row(Rest, NewAcc).
+
+chunk_generator(<<>>) ->
+    {<<>>, done};
+chunk_generator(Csv) ->
+    csv_binary:split_by_size(Csv, random_chunk_size()).
+
+random_chunk_size() ->
+    ChunkSizes = [0, 1, 2, 3, 7],
+    lists:nth(rand:uniform(length(ChunkSizes)),
+              ChunkSizes).
